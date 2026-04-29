e8de65f3cceaa6f84d6cd51395789f289fbc15d4
xxpurplehazexx1337-coder <xxpurplehazexx.1337@gmail.com>
2026-04-28 21:31:18 -0600

    Update asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -12,4 +12,5 @@ static char* asciiart = "\n"
 "   \\_____\\____/|_| \\_|_____/ \\____/|______|______|_____/\n\n";
 
 static char* asciitail =
-"              POWERED BY Kevin's XeLL RELOADED - Xenon Linux Loader\n\n";
+"              POWERED BY Kevin's XeLL RELOADED - Xenon Linux Loader\n"
+"  Copyright (C) 2001-2010 Microsoft Corporation. All rights reserved.\n\n";
